45e03f0ed197c75e39d0a6b63f61099c33a730e6
Emanuele Ciurleo <emanuele.ciurleo@surevine.com>
2022-09-29 09:47:52 +0100

    Revert "Moved common functions to TestBase"
    This reverts commit 7ece1ca5b6428ca96042a1f42e45ec9bcd677a78.

--- src/main/java/tests/GoogleHomeTest.java
+++ src/main/java/tests/GoogleHomeTest.java
@@ -1,21 +1,34 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.asserts.SoftAssert;
-import utils.TestBase;
 
 
-public class GoogleHomeTest extends TestBase {
-
-    //URL to start everything off from
-    public void gotoBaseURL() {
-        driver.get("http://www.google.com");
+public class GoogleHomeTest {
+    WebDriver driver;
+    @BeforeSuite
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+    @BeforeClass
+    void setup() {
+        driver = new ChromeDriver();
+    }
+    @AfterClass
+    void teardown() {
+        driver.quit();
     }
 
-    //Test start here
     @Test (description = "Is the Google Page Title correct")
     public void pageTitleTest() {
-        gotoBaseURL();
+        driver.get("http://www.google.com"); //TODO move to base URL
         SoftAssert sa = new SoftAssert();
         sa.assertEquals(driver.getTitle(),"Google");
         sa.assertAll();
